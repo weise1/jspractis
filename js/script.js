b55bf276733e9c9f51012deed9d4fb0d5bb2cfe9
max <example@gmail.com>
2022-08-04 22:40:16 +0300

    fist coommit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+console.log('arr' + ' - obj');
+console.log(4 + '5');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr, --decr);
+// console.log(decr);
+
+
+console.log(5 % 2);
+
+console.log(2*4 === 8);
+
+
+const isCheked = false,
+      isClose = false;
+
+console.log(isCheked || !isClose);
+
+
+
+
+
+
+
